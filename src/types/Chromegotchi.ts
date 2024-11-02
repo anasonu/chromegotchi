2838d9c92c360ddf05148fa2f804a9fcde1d2d51
Ana Sobrino <anasonu@hotmail.com>
2024-11-02 20:00:33 +0100

    Create profile section

--- src/types/Chromegotchi.ts
+++ src/types/Chromegotchi.ts
@@ -8,17 +8,18 @@ export interface Chromegotchi {
     happiness: number;
     evolves: string;
     gets_sick: string;
-    hh_timer: string;
+    hh_timer: number;
     min_weight: number;
     discipline: number;
-    discipline_timer: string;
+    discipline_timer: number;
     care_mistakes: number;
-    illness_timer: string;
+    illness_timer: number;
     is_sick: false;
     is_asleep: false;
     sleep_time: string;
     waking_time: string;
     injections: number;
+    // lastFeeded: string;
   }
 
   export const defaultChromegotchis: Chromegotchi[] = [
@@ -30,17 +31,18 @@ export interface Chromegotchi {
       happiness: 0,
       evolves: addMinutes(new Date(), 5),
       gets_sick: "",
-      hh_timer: "",
+      hh_timer: 0,
       min_weight: 1,
       discipline: 0,
-      discipline_timer: "",
+      discipline_timer: 0,
       care_mistakes: 0,
-      illness_timer: "",
+      illness_timer: 0,
       is_sick: false,
       is_asleep: false,
       sleep_time: "",
       waking_time: "",
       injections: 0,
+      // lastFeeded: ""
     },
     {
       id: 1,
@@ -50,16 +52,17 @@ export interface Chromegotchi {
       happiness: 0,
       evolves: addMinutes(new Date(), 60),
       gets_sick: "",
-      hh_timer: "7",
+      hh_timer: 3,
       min_weight: 2,
       discipline: 0,
-      discipline_timer: "",
+      discipline_timer: 0,
       care_mistakes: 0,
-      illness_timer: "",
+      illness_timer: 0,
       is_sick: false,
       is_asleep: false,
       sleep_time: "40min de nacer",
       waking_time: "5min de dormir",
       injections: 0,
+      // lastFeeded: ""
     }
   ]
